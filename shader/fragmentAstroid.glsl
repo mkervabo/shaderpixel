67ac4194eeca0daad3b80cea7726725d4053e3cb
gperez <gperez@student.42.fr>
2021-09-28 21:26:06 +0200

    Ajouts des nouveaux shader mais pas encore fonctionnel par rapport au projet

---
+++ shader/fragmentAstroid.glsl
@@ -0,0 +1,276 @@
+const int MAX_ITERATIONS = 10;
+const int MAX_STEPS = 100;
+const int MAX_AO_STEPS = 10;
+const float MIN_DIST = 0.001;
+const float MAX_DIST = 10.;
+const float EPSILON = 0.001;
+
+const vec3 COLOR_SPHERE = vec3(0.6, 0.35, 0.2);
+
+#define K_A 0.2
+#define K_SHIN 3.
+#define K_S 0.3
+#define K_D 0.8
+#define IT_SHADOWS 100
+
+struct s_light
+{
+	vec3 pos;
+	vec3 colorLight;
+	float intensity;
+};
+
+
+// http://iquilezles.org/www/articles/smin/smin.htm
+float smin( float a, float b, float k )
+{
+	float h = max(k-abs(a-b),0.0);
+	return min(a, b) - h*h*0.25/k;
+}
+
+// http://iquilezles.org/www/articles/smin/smin.htm
+float smax( float a, float b, float k )
+{
+	float h = max(k-abs(a-b),0.0);
+	return max(a, b) + h*h*0.25/k;
+}
+
+float random (in vec2 st) {
+	return fract(sin(dot(st.xy,
+						 vec2(12.9898,78.233)))*
+		43758.5453123);
+}
+
+vec4 unionSDF(vec4 a, vec4 b)
+{
+	return (a.w < b.w ? a : b);
+}
+
+float sphereDE(vec3 p, float rayon)
+{
+	return (length(p) - rayon);
+}
+
+float hash(vec3 p)  
+{
+	p  = 17.0*fract( p*0.3183099+vec3(.11,.17,.13) );
+	return fract( p.x*p.y*p.z*(p.x+p.y+p.z) );
+}
+
+float sph( vec3 i, vec3 f, vec3 c )
+{
+   
+   // random radius at grid vertex i+c
+   float rad = hash(i + c) * 0.55;
+   // distance to sphere at grid vertex i+c
+   return length(f - c) - rad; 
+		
+}
+
+float sdBase(vec3 p)
+{
+	vec3 i = floor(p);
+	vec3 f = fract(p);
+	// distance to the 8 corners spheres
+	return min(min(min(sph(i,f,vec3(0,0,0)),
+				  sph(i,f,vec3(0,0,1))),
+				  min(sph(i,f,vec3(0,1,0)),
+					  sph(i,f,vec3(0,1,1)))),
+			  min(min(sph(i,f,vec3(1,0,0)),
+					  sph(i,f,vec3(1,0,1))),
+				  min(sph(i,f,vec3(1,1,0)),
+					  sph(i,f,vec3(1,1,1)))));
+}
+
+#define I_TERRAIN 8
+
+vec2 sdFbm( vec3 p, float d)
+{
+	float s = 1.0;
+	float t = 0.;
+	for (int i = 0; i < I_TERRAIN; i++)
+	{
+		// evaluate new octave
+		float n = s*sdBase(p);
+		d = smax(d, -n, 0.2 * s);
+		t += d;
+		// prepare next octave
+		p = mat3( 0.00, 1.60, 1.20,
+				-1.60, 0.72,-0.96,
+				-1.20,-0.96, 1.28 )*p;
+		s = 0.5 * s;
+	}
+	return (vec2(d, t));
+}
+
+vec2 DistanceEstimation(vec3 p)
+{
+	float sphere = sphereDE(p, 0.5);
+		
+	vec2 dt = sdFbm(p, sphere);
+	dt.y = 1.0 + dt.y * 2.5;
+	dt.y = dt.y * dt.y;
+
+	return (dt);
+}
+
+vec2 iBox( in vec3 ro, in vec3 rd, in vec3 rad )
+{
+	vec3 m = 1.0/rd;
+	vec3 n = m*ro;
+	vec3 k = abs(m)*rad;
+	vec3 t1 = -n - k;
+	vec3 t2 = -n + k;
+	float tN = max( max( t1.x, t1.y ), t1.z );
+	float tF = min( min( t2.x, t2.y ), t2.z );
+	if( tN > tF || tF < 0.0)
+		return vec2(-1.0);
+	return vec2(tN, tF);
+}
+
+vec2 ShortestDistanceToSurface(vec3 eyeP, vec3 marchinDir, float start,
+	float end)
+{
+	vec2 ret;
+	//bounding volume
+	vec2 dis = iBox(eyeP, marchinDir, vec3(1.0));
+	if( dis.y < 0.0 )
+		return vec2(end, 0.);
+
+	float depth = dis.x;
+	vec2 h;
+	for (int i = 0; i < MAX_STEPS; i++)
+	{
+		h = DistanceEstimation(eyeP + marchinDir * depth);
+		ret.x = depth;
+		ret.y = h.y;
+		if (h.x < EPSILON ||  depth > dis.y)
+			break;
+		depth += h.x;
+	}
+	if (depth > dis.y) 
+		ret = vec2(end, 0.);
+	return (ret);
+}
+
+vec3 calculateMarchinDir(float fov, vec2 resolutionSize, vec2 fragCoord)
+{
+	vec2 xy = fragCoord - resolutionSize / 2.0;
+	float z = resolutionSize.y / tan(radians(fov));
+	return (normalize(vec3(xy, -z)));
+}
+
+vec3 estimateNormal(vec3 p)
+{
+	return (normalize(vec3(DistanceEstimation(vec3(p.x + EPSILON, p.y, p.z)).x
+		- DistanceEstimation(vec3(p.x - EPSILON, p.y, p.z)).x,
+			DistanceEstimation(vec3(p.x, p.y + EPSILON, p.z)).x
+		- DistanceEstimation(vec3(p.x, p.y - EPSILON, p.z)).x,
+			DistanceEstimation(vec3(p.x, p.y, p.z + EPSILON)).x
+		- DistanceEstimation(vec3(p.x, p.y, p.z - EPSILON)).x)));
+}
+
+float calcSoftShadow(vec3 ro, vec3 rd, float tmin, float tmax, float w)
+{
+	vec2 dis = iBox(ro, rd, vec3(1.0));
+	if (dis.y < 0.0 )
+		return (1.0);
+		
+	tmin = max(tmin,dis.x);
+	tmax = min(tmax,dis.y);
+		
+	float t = tmin;
+	float res = 1.0;
+	for (int i = 0; i < IT_SHADOWS; i++)
+	{
+		float h = DistanceEstimation(ro + rd * t).x;
+		res = min( res, h/(w*t) );
+		t += clamp(h, 0.005, 1.);
+		if (res < -1.0 || t > tmax)
+			break;
+	}
+	return (res);
+}
+
+vec3 phongLight(s_light light, vec3 eye, vec3 dir, vec3 norm, vec3 pos, float mat)
+{
+	vec3 colorObj = mix(COLOR_SPHERE, vec3(1.), mat ) * 0.7;;
+		
+	vec3 vPToEye = normalize(eye - pos);
+	vec3 vPL = normalize(light.pos - pos);
+	float diffuse = dot(vPL, norm) * K_D;
+		
+	vec3 reflL = reflect(vPL, norm);
+	float specular = dot(-reflL, vPToEye);
+
+	if (diffuse < EPSILON)
+		return (vec3(0., 0., 0.));
+
+	diffuse *= calcSoftShadow(pos + norm * 0.01, normalize(light.pos), MIN_DIST, MAX_DIST, 0.003);
+	diffuse = clamp(diffuse, 0., 1.);
+	vec3 ret;
+	ret += light.intensity * (colorObj * 1.5 *vec3(1.,0.85,0.75) * diffuse);
+
+	if (specular < EPSILON || diffuse < 0.1)
+		return (ret);
+	ret += light.intensity * (light.colorLight * pow(specular, K_SHIN) * K_S);
+	return (ret);
+}
+
+vec3 calculateColor(s_light light, vec3 eye, vec3 dir, vec3 pos, vec3 norm, float mat)
+{
+	vec3 color;
+		
+	color += phongLight(light, eye, dir, norm, pos, mat);
+	color += K_A * vec3(0.40,0.45,0.60) * mat * mat * mat * (0.1 * norm.y);
+
+	//Reinhard tonemaping https://www.shadertoy.com/view/lslGzl
+	float exposure = 1.5;
+	color *= exposure / (1. + color / exposure);
+
+	//gamma
+	color = pow(color, vec3(0.4545));
+	return (color);
+}
+
+mat4 viewMatrix(vec3 eye, vec3 pToLook, vec3 up)
+{
+	vec3 front = normalize(pToLook - eye);
+	vec3 right = normalize(cross(front, up));
+	vec3 newUp = normalize(cross(right, front));
+	return (mat4(
+		vec4(right, 0.),
+		vec4(newUp, 0.),
+		vec4(-front, 0.),
+		vec4(vec3(0.0), 1.0)
+	));
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+	vec3 dir = calculateMarchinDir(45., iResolution.xy, fragCoord );
+	vec3 eye = vec3(cos(iTime * 0.025), 1., sin(iTime * 0.025));
+
+	mat4 viewToWorld = viewMatrix(eye, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
+		
+	vec3 worldDir = (viewToWorld * vec4(dir, 0.0)).xyz;
+		
+	vec2 dt = ShortestDistanceToSurface(eye, worldDir, MIN_DIST, MAX_DIST);
+
+	vec3 posHit = eye + worldDir * dt.x;
+	vec3 norm = estimateNormal(posHit);
+
+	if (dt.x > MAX_DIST - EPSILON)
+	{
+		fragColor = vec4(0.);
+		return ;
+	}
+	s_light light;
+	light.pos = vec3(cos(iTime * 0.1), 2., sin(iTime * 0.1));
+	light.colorLight = vec3(1.0, 1.0, 1.0);
+	light.intensity = 1.;
+		
+	vec3 color = calculateColor(light, eye, worldDir, posHit, norm, dt.y);
+
+	fragColor = vec4(color, 1.0);
+}
---
+++ shader/fragmentCloud.glsl
@@ -0,0 +1,487 @@
+const int MAX_REFLECTIONS = 1;
+const int MAX_STEPS = 400;
+const int MAX_STEPS_REF = 100;
+const int MAX_STEPS_DENSITY = 64; // 32
+const int MAX_STEPS_SHADOWS = 16; // 32
+const int MAX_AO_STEPS = 32;
+const float MIN_DIST = 0.1;
+const float MAX_DIST = 100.0;
+const float EPSILON = 0.0025;
+const float EPSILON_REF = 0.0025;
+const int T_SIZEX = 12;
+const int T_SIZEY = 12;
+
+const float BOUND = .5;
+
+#define K_A 0.4
+#define K_S 50.
+#define K_D 1.
+#define K_R 0.2
+#define INCIDENCE 1.02
+#define K_T 0.5
+#define K_LIGHT 5.
+#define DENS_F 2. // 1.
+#define SHAD_DENS_F 5.5 // 3.5
+#define THRESHOLD 0.8 // 0.8
+
+vec3 rotY(vec3 p, float theta)
+{
+	return (p * mat3(
+		vec3(cos(theta), 0., sin(theta)),
+		vec3(0., 1., 0.),
+		vec3(-sin(theta), 0., cos(theta))));
+}
+
+struct s_light
+{
+	vec3 pos;
+	vec3 colorLight;
+	float intensity;
+};
+
+struct s_obj
+{
+	int type;
+	vec4 color;
+	float dist;
+};
+
+struct s_hit
+{
+	vec3 posHit;
+	int typeHit;
+	float dist;
+};
+
+struct s_ray
+{
+	vec3 eyeP;
+	vec3 dir;
+	vec3 localCam;
+	float w;
+	mat4 viewToWorld;
+};
+
+struct s_env
+{
+	s_ray ray;
+	s_light light;
+	bool isDifSpecSha[3];
+	int typeHit;
+};
+
+s_obj unionSDF(s_obj obj1, s_obj obj2)
+{
+	if (obj1.dist < obj2.dist)
+		return (obj1);
+	return (obj2);
+}
+
+float sphereDE(vec3 p, float rayon)
+{
+	return (length(p) - rayon);
+}
+
+float boxDE(vec3 p, vec3 b)
+{
+	vec3 q = abs(p) - b;
+	return (length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0));
+}
+
+float planDE( vec3 p, vec3 n, float h )
+{
+	// n must be normalized
+	return dot(p,n) + h;
+}
+
+#define NONE -1
+#define CLOUD 0
+#define CUBE 1
+#define SPHERE 2
+#define SPHERE_2 3
+
+s_obj DistanceEstimation(vec3 p, int typeTouch)
+{
+	s_obj cube;
+	cube.type = CUBE;
+	cube.color = vec4(0.1, 0.5, 0.7, 1.);
+	cube.dist = boxDE(p + vec3(-2., 0.5, 0.), vec3(1.));
+
+	s_obj sphere;
+	sphere.type = SPHERE;
+	sphere.color = vec4(0.1, 0.5, 0.7, 1.);
+	sphere.dist = sphereDE(vec3(-1., -0.25, 0.) + rotY(p, iTime * 0.5), 0.1);
+	s_obj scene = unionSDF(cube, sphere);
+
+	s_obj sphere2;
+	sphere2.type = SPHERE_2;
+	sphere2.color = vec4(0.5, 0.1, 0.7, 1.);
+	sphere2.dist = sphereDE(p + vec3(0., 0., -0.3 + cos(iTime * 0.5)), 0.1);
+	scene = unionSDF(scene, sphere2);
+
+	if (typeTouch == CLOUD)
+		return (scene); 
+
+	s_obj cloud;
+	cloud.type = CLOUD;
+	cloud.color = vec4(1.);
+	cloud.dist = boxDE(p, vec3(BOUND));
+
+	scene = unionSDF(cloud, scene);
+	return (scene);
+}
+
+vec3 estimateNormal(vec3 p, int typeHit)
+{
+	return (normalize(vec3(DistanceEstimation(vec3(p.x + EPSILON, p.y, p.z), typeHit).dist
+		- DistanceEstimation(vec3(p.x - EPSILON, p.y, p.z), typeHit).dist,
+			DistanceEstimation(vec3(p.x, p.y + EPSILON, p.z), typeHit).dist
+		- DistanceEstimation(vec3(p.x, p.y - EPSILON, p.z), typeHit).dist,
+			DistanceEstimation(vec3(p.x, p.y, p.z + EPSILON), typeHit).dist
+		- DistanceEstimation(vec3(p.x, p.y, p.z - EPSILON), typeHit).dist)));
+}
+
+///// DENSITY /////
+vec2 convert1dTo2d(float xsize, float index)
+{
+	vec2 xyIdx;
+		
+	xyIdx.x = mod(index, xsize);
+	xyIdx.y = xsize - floor(index / xsize);
+	return (xyIdx);
+}
+
+vec4 pseudoVolumeTexture(vec3 pos)
+{
+	//float att = 30. + 10. * sin(iTime);
+	pos = pos + 0.5;// + 0.5; // / 2. permet le grossissement et 0.5 permet de le centré (le centre du cube est en 0.)
+	//pos = floor(pos * att) / att;
+	pos.z += 0.06;
+	pos.x = clamp(pos.x, 0., 1.);
+	pos.y = clamp(pos.y, 0., 1.);
+	pos.z = clamp(pos.z, 0.075, 1.09);
+		
+	float xsize = float(T_SIZEX); // 12
+	float numframes = xsize * float(T_SIZEY); // 12
+		
+	float zphase = fract(pos.z * numframes);
+	float zframe = pos.z * numframes - zphase;
+
+	vec2 uv = fract(pos.xy);
+	uv /= xsize;
+
+	vec2 curframe = convert1dTo2d(xsize, zframe) / xsize;
+	vec2 nextframe = convert1dTo2d(xsize, zframe + 1.) / xsize;
+
+	vec4 sampleA = textureLod(iChannel0, uv + curframe, 0.);
+	vec4 sampleB = textureLod(iChannel0, uv + nextframe, 0.);
+
+	return mix(sampleA, sampleB, zphase);
+}
+
+struct s_density
+{
+	vec3 lightEnergy;
+	float transmittance;
+	float distT;
+};
+
+struct s_cloud
+{
+	float curDensity;
+	float curVolTxt;
+		
+	vec3  lightEnergy;
+	float transmittance;
+		
+	float stepSize;
+	float shadowStepSize;
+		
+	vec3 lPos;
+	vec3 curPos;
+		
+	vec3 inPos;
+	vec3 outPos;
+};
+
+void shadowCloud(inout s_cloud cloud, vec3 lightDir, float sh)
+{
+	float DensityFactor = DENS_F * cloud.stepSize;
+	float ShadowDensityFactor = SHAD_DENS_F * cloud.shadowStepSize;
+	float shadowtransmittance = 0.;
+	vec3	absorbedLight = vec3(0.);
+	
+	float MaxShadowDist = float(MAX_STEPS_SHADOWS) / 1.5;
+	float shadowDist;
+	float curShadowVolTxt;
+	
+	shadowDist = 0.;
+	cloud.lPos = cloud.curPos;
+	if (sh < 0.1) // S'il y a de l'ombre sur le nuage
+		shadowDist = MaxShadowDist;
+	else
+	{
+		for (int iS = 0; iS < MAX_STEPS_SHADOWS; iS++)
+		{
+			cloud.lPos += lightDir;
+			curShadowVolTxt = pseudoVolumeTexture(cloud.lPos).r;
+			shadowDist += curShadowVolTxt;
+
+			if (cloud.lPos.x > BOUND || cloud.lPos.x < -BOUND
+				|| cloud.lPos.y > BOUND || cloud.lPos.y < -BOUND
+					|| cloud.lPos.z > BOUND || cloud.lPos.z < -BOUND) break;
+		}
+	}
+	cloud.curDensity = min(cloud.curVolTxt * DensityFactor, 1.);
+	shadowtransmittance = exp(-shadowDist * ShadowDensityFactor); // t = e^(-t * d)
+	absorbedLight = vec3(shadowtransmittance * cloud.curDensity);
+	
+	cloud.lightEnergy += absorbedLight * cloud.transmittance;
+	cloud.transmittance *= 1. - cloud.curDensity;
+}
+
+s_density ambiantCloud(inout s_cloud cloud)
+{
+	float shadowDist = 0.;
+
+	cloud.lPos = cloud.curPos + vec3(0,0,0.05);
+	float lsample = pseudoVolumeTexture(cloud.lPos).r;
+	shadowDist += lsample;
+	cloud.lPos = cloud.curPos + vec3(0,0,0.1);
+	lsample = pseudoVolumeTexture(cloud.lPos).r;
+	shadowDist += lsample;
+	cloud.lPos = cloud.curPos + vec3(0,0,0.2);
+	lsample = pseudoVolumeTexture(cloud.lPos).r;
+	shadowDist += lsample;
+
+	float AmbientDensity = 0.3;
+	vec3 SkyColor = vec3(0.57, 0.75, 0.94);
+		
+	cloud.lightEnergy += exp(-shadowDist * AmbientDensity) * cloud.curDensity * SkyColor * cloud.transmittance;
+	return (s_density(cloud.lightEnergy * K_LIGHT, cloud.transmittance, distance(cloud.outPos, cloud.inPos)));
+}
+
+s_density getDensity(s_env env, vec3 curP, vec3 dir, vec3 lightDir, float sh)
+{
+	s_cloud cloud = s_cloud(0.,0., vec3(0.),1.,
+		1. / float(MAX_STEPS_DENSITY), 1. / float(MAX_STEPS_SHADOWS), vec3(0.), curP, curP, vec3(0.));
+		
+	lightDir *= cloud.shadowStepSize;
+	dir *= cloud.stepSize;
+	for (int i = 0; i < MAX_STEPS_DENSITY; i++)
+	{
+		cloud.curVolTxt = pseudoVolumeTexture(cloud.curPos).r;
+		if (cloud.curVolTxt > 0.01) // light calcul
+			shadowCloud(cloud, lightDir, sh); //
+		cloud.curPos += dir;
+		if (cloud.transmittance < THRESHOLD) break;
+	}
+	cloud.outPos = cloud.curPos;
+	return (ambiantCloud(cloud));
+}
+
+float getOnlyDensity(vec3 curP, vec3 dir)
+{
+	float stepSize = 1. / float(MAX_STEPS_DENSITY);
+	float Density = DENS_F;
+	float curDens = 0.;
+	float curVolTxt;
+
+	dir *= stepSize;
+	for (int i = 0; i < MAX_STEPS_DENSITY; i++)
+	{
+		curVolTxt = pseudoVolumeTexture(curP).r;
+		curDens += curVolTxt;
+		curP += dir;
+	}
+	return min(curDens * stepSize * DENS_F, 1.);
+}
+
+///// DENSITY /////
+
+float softShadow(s_env env, float mint, float maxt, float k, int type)
+{
+	float h;
+	s_obj obj;
+	float res = 1.0;
+	vec3 p;
+
+	for( float t=mint; t<maxt; )
+	{
+		p = env.ray.eyeP + env.ray.dir * t;
+		obj = DistanceEstimation(p, type);
+		h = obj.dist;
+		if (h < EPSILON)
+		{
+			if (obj.type == CLOUD)
+			{
+				res = (1. - getOnlyDensity(p, env.ray.dir));
+				type = CLOUD;
+			}
+			else
+				return 0.;
+		}
+		if (obj.type != CLOUD)
+			res = min(res, k * h / t);
+		t += h;
+	}
+	return res;
+}
+
+vec3 phongLight(s_light light, vec3 vEP, vec3 norm, vec3 pos, bool isDifSpecSha[3], vec3 colorObj)
+{
+	vec3 vPL = normalize(light.pos - pos);
+	vec3 vReflectLN = normalize(reflect(-vPL, norm));
+	 
+	float diffuse = dot(vPL, norm);
+	float specular = dot(vReflectLN, vEP);
+		
+	if (diffuse < EPSILON)
+		return (vec3(0., 0., 0.));
+	if (specular < EPSILON)
+		return (light.intensity * (colorObj * diffuse * K_D));
+
+	vec3 ret;
+		
+	if (isDifSpecSha[0]) // diffuse
+		ret += light.intensity * (colorObj * diffuse * K_D);
+	if (isDifSpecSha[1]) // specular
+		ret += light.intensity * (light.colorLight * pow(specular, K_S));
+	return (ret);
+}
+
+float calcShadow(s_env env, vec3 pos, int type)
+{
+	env.ray.eyeP = pos;
+	env.ray.dir = normalize(env.light.pos - pos);
+	float sh = softShadow(env, 0.,
+		distance(env.light.pos, pos), 30., type);
+	return (sh);
+}
+
+vec3 calculateColor(s_env env, vec3 pos, vec3 norm, vec3 colorObj)
+{
+	vec3 ambiantLight = colorObj * K_A;
+	vec3 vEP = normalize(env.ray.eyeP - pos);
+	vec3 colorShadow = vec3(0.);
+	vec3 color;
+		
+	color += phongLight(env.light, vEP, norm, pos, env.isDifSpecSha, colorObj);
+		
+	pos += norm * 0.01;
+	if (env.isDifSpecSha[2])
+	{
+		float sh = calcShadow(env, pos, NONE);
+		color *= clamp(sh, 0., 1.);
+	}
+	color += ambiantLight;
+	return (color);
+}
+
+s_hit ShortestDistanceToSurface(s_env env, float start, float end, inout vec4 dColorObj)
+{
+	vec3 color;
+	float depth = start;
+	vec3 newPos;
+	vec3 norm;
+	s_hit hit;
+	s_obj obj;
+	float coef = 1.;
+	float distT = 0.;
+	vec3 inPosCloud;
+		
+	hit.typeHit = -1;
+	for (int i = 0; i < MAX_STEPS; i++)
+	{
+		newPos = env.ray.eyeP + env.ray.dir * depth;
+		obj = DistanceEstimation(newPos, hit.typeHit);
+		if (obj.dist < EPSILON) // Si on touche un obj
+		{
+			color = obj.color.rgb;
+			hit.posHit = newPos;
+			hit.dist = depth;
+			hit.typeHit = obj.type;
+			if (obj.type == CLOUD) // Si on touche le nuage
+			{
+				inPosCloud = hit.posHit;
+				norm = estimateNormal(hit.posHit, NONE);
+				float sh = calcShadow(env, hit.posHit + norm * 0.01, hit.typeHit);
+			
+				s_density density = getDensity(env, hit.posHit, env.ray.dir,
+					normalize(env.light.pos - hit.posHit), sh);
+				dColorObj.rgb = env.light.colorLight * (1. - density.transmittance)
+					+ density.lightEnergy * density.transmittance;
+				coef = density.transmittance;
+				distT = density.distT;
+			}
+			else
+			{
+				norm = estimateNormal(hit.posHit, NONE);
+				if (distT > 0.01) // Si un objet et devant ou derriere le nuage (permet d'augmenter l'opacité en fonction de la profondeur)
+				{ 
+					float d = 0.;
+					
+					if (coef < 0.9)
+						d = min(distance(hit.posHit, inPosCloud), distT) / distT;
+					dColorObj.rgb += calculateColor(env, hit.posHit, norm, color).rgb * coef * (1. - d);
+				}
+				else
+					dColorObj.rgb += calculateColor(env, hit.posHit, norm, color).rgb * coef;
+				return (hit);
+			}
+		}
+		depth += obj.dist;
+		if (depth > end - EPSILON) // Si aucun obj n'est detecter
+		{
+			dColorObj += vec4(0.);
+			//dColorObj += texture(iChannel1, env.ray.dir);
+			hit.dist = end;
+			return (hit);
+		}
+	}
+	hit.dist = end;
+	dColorObj += vec4(0.);
+	//dColorObj += texture(iChannel1, env.ray.dir);
+	return (hit);
+}
+
+vec3 calculateMarchinDir(float fov, vec2 resolutionSize, vec2 fragCoord)
+{
+	vec2 xy = fragCoord - resolutionSize / 2.0;
+	float z = resolutionSize.y / tan(radians(fov / 2.));
+	return (normalize(vec3(xy, -z)));
+}
+
+mat4 viewMatrix(vec3 eye, vec3 pToLook, vec3 up)
+{
+	vec3 front = normalize(pToLook - eye);
+	vec3 right = normalize(cross(front, up));
+	vec3 newUp = normalize(cross(right, front));
+	return (mat4(
+		vec4(right, 0.),
+		vec4(newUp, 0.),
+		vec4(-front, 0.),
+		vec4(vec3(0.0), 1.0)
+	));
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+	s_env env;
+	env.ray.localCam = calculateMarchinDir(45., iResolution.xy, fragCoord);
+	env.ray.w = 1.;
+	env.ray.eyeP = vec3(7. * cos(iTime * .25) , 0., 7. * sin(iTime * .25));
+	env.isDifSpecSha = bool[3](true, true, true);
+
+	env.ray.viewToWorld = viewMatrix(env.ray.eyeP, vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0));
+
+	env.ray.dir = (env.ray.viewToWorld * vec4(env.ray.localCam, 0.0)).xyz; // worldDir
+
+	vec4 dColorObj = vec4(vec3(0.), 1.);
+
+	env.light.pos = vec3(-4., 2., -1.);
+	env.light.colorLight = vec3(0.25, 0., 0.45);
+	env.light.intensity = 0.5;
+	s_hit hit = ShortestDistanceToSurface(env, MIN_DIST, MAX_DIST, dColorObj);
+	fragColor = dColorObj;
+}
