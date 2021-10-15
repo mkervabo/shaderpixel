3f80143df39a40e125a0c267171d048fcd838df5
gperez <gperez@student.42.fr>
2021-10-15 11:34:34 +0200

    Le mandelbulb rends mieux et ne se deforme plus avec la distance. Un modelPos a ete ajouté

--- srcs/Class/Shaderpixel.cc
+++ srcs/Class/Shaderpixel.cc
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Shaderpixel.cc                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maiwenn <maiwenn@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gperez <gperez@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 15:39:27 by gperez            #+#    #+#             */
-/*   Updated: 2021/10/15 09:52:43 by maiwenn          ###   ########.fr       */
+/*   Updated: 2021/10/15 10:44:01 by gperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -155,10 +155,10 @@ bool				Shaderpixel::init(void)
 		return (1);
 	if (load(E_PBALL, VERTEX_LIGHT, FRAGMENT_LIGHT, E_DEFAULT_MESH)
 		|| load(E_PBALL, VERTEX, FRAGMENT, E_DEFAULT_MESH)
-		|| load(E_PPLANE, VERTEX_RENDERBUFFER, FRAGMENT_RENDER_BUFFER, E_RENDER_BUFFER))
+		|| load(E_PCUBE, VERTEX_MANDELBULB, FRAGMENT_MANDELBULB, E_FRACTAL))
 			return (1);
 	this->meshes[1]->translate(Vec3(0., 0., -3.5));
-	this->meshes[2]->rotate(Vec3(90., 0., 0.));
+	// this->meshes[2]->rotate(Vec3(90., 0., 0.));
 
 	// std::cout << this->meshes[0]->getShaderProgram() << " " << this->meshes[1]->getShaderProgram() << "\n";
 	this->time.setTime();
